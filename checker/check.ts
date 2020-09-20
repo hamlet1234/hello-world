d7678503e10ccd4ed70d048556bd5b90cfdd3c66
Hamlet <hamlet.mikaelyan@codesignal.com>
2020-09-20 17:23:38 +0400

    action

---
+++ checker/check.ts
@@ -0,0 +1,2 @@
+console.log('blah');
+process.exit(Math.floor(Math.random() * 2));
---
+++ checker/checkAll.ts
@@ -0,0 +1,8 @@
+const { exec } = require('child_process');
+for (let i = 0; i < 10; ++i) {
+  exec('ts-node check.ts', (error, stdout, stderr) => {
+    if (error) {
+      console.log(i);
+    }
+  });
+}
