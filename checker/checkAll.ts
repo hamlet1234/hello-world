3d665233b4243ebd4add4efc1b84926de8f21798
Hamlet <hamlet.mikaelyan@codesignal.com>
2020-09-21 03:06:35 +0400

    return error

--- checker/checkAll.ts
+++ checker/checkAll.ts
@@ -1,8 +1,13 @@
 const { exec } = require('child_process');
+let ok = true;
 for (let i = 0; i < 10; ++i) {
   exec('ts-node check.ts', (error, stdout, stderr) => {
     if (error) {
       console.log(i);
+      ok = false;
     }
   });
+}
+if (!ok) {
+  process.exit(1);
 }
